bd2a420e2faf5e5cca4d9d20f8da1f0d89c0f1af
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-10-17 23:51:01 +0300

    tresh

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */
+
+
+// galleryItems.forEach(element => {
+   
+//     console.log("111", element)
+//     return element
+// });
+
+
+const galleryRef = document.querySelector(".gallery")
+
+
+for (let i = 1; i <= galleryItems.length; i++) {
+	let galleryItem = document.createElement("div")
+
+     galleryItem.classList.add("gallery__item")
+
+    const galleryLink= document.createElement("a")
+      galleryLink.classList.add("gallery__link");
+      galleryLink.href = `sha256-nMn34BfOxpKD0GwV5nZMwdS4e8SI8Ekz+G7dLeGE4XY=`
+      galleryItem.append(galleryLink)
+
+
+
+     galleryRef.append(galleryItem)
+}
+
+
+
+
+
+console.log(galleryRef)
+
+console.log(galleryItems.length);
